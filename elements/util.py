94478719485608904f9a63424249897bd0716ba9
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-04-21 22:27:54 +0200

    #16 perf Suppression d'issues détectées par sonar

--- elements/util.py
+++ elements/util.py
@@ -28,7 +28,7 @@ def double_right_linear_traverse(turtle):
 def left_square(turtle):
     turtle.down()
     turtle.begin_fill()
-    for i in range(4):
+    for _ in range(4):
         turtle.forward(20)
         turtle.left(90)
     turtle.end_fill()
@@ -36,7 +36,7 @@ def left_square(turtle):
 def right_square(turtle):
     turtle.down()
     turtle.begin_fill()
-    for i in range(4):
+    for _ in range(4):
         turtle.forward(20)
         turtle.right(90)
     turtle.end_fill()
@@ -44,7 +44,7 @@ def right_square(turtle):
 def row_left_squares(turtle, number):
     turtle.down()
     turtle.begin_fill()
-    for i in range(number):
+    for _ in range(number):
         left_square(turtle)
         turtle.forward(20)
     turtle.end_fill()
@@ -52,7 +52,7 @@ def row_left_squares(turtle, number):
 def row_right_squares(turtle, number):
     turtle.down()
     turtle.begin_fill()
-    for i in range(number):
+    for _ in range(number):
         right_square(turtle)
         turtle.forward(20)
     turtle.end_fill()
