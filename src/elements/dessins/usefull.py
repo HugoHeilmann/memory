e923d5e08ed263637c0a999d76efa56319232cf1
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-04-24 17:37:12 +0200

    #7 feat Création d'un bob-omb

--- src/elements/dessins/usefull.py
+++ src/elements/dessins/usefull.py
@@ -1,68 +1,68 @@
-def low_traverse(turtle):
-    turtle.forward(20)
-    turtle.left(90)
-    turtle.forward(20)
+def low_traverse(t):
+    t.forward(20)
+    t.left(90)
+    t.forward(20)
 
-def long_left_diagonal_from_above(turtle):
-    turtle.forward(40)
-    turtle.left(90)
-    turtle.forward(20)
+def long_left_diagonal_from_above(t):
+    t.forward(40)
+    t.left(90)
+    t.forward(20)
 
-def from_above(turtle):
-    turtle.right(90)
-    turtle.forward(60)
-    turtle.right(90)
+def from_above(t):
+    t.right(90)
+    t.forward(60)
+    t.right(90)
 
-def walk_along_up_square(turtle):
-    turtle.right(90)
-    turtle.forward(20)
-    turtle.left(90)
+def walk_along_up_square(t):
+    t.right(90)
+    t.forward(20)
+    t.left(90)
 
-def long_left_diagonal(turtle):
-    turtle.left(90)
-    turtle.forward(20)
-    turtle.right(90)
-    turtle.forward(40)
+def long_left_diagonal(t):
+    t.left(90)
+    t.forward(20)
+    t.right(90)
+    t.forward(40)
 
-def double_high_traverse(turtle):
-    turtle.forward(20)
-    turtle.right(90)
-    turtle.forward(40)
-    turtle.left(90)
+def double_high_traverse(t):
+    t.forward(20)
+    t.right(90)
+    t.forward(40)
+    t.left(90)
 
-def double_right_linear_traverse(turtle):
-    turtle.right(90)
-    turtle.forward(40)
-    turtle.right(90)
+def double_right_linear_traverse(t):
+    t.right(90)
+    t.forward(40)
+    t.right(90)
 
-def left_square(turtle):
-    turtle.down()
-    turtle.begin_fill()
+def left_square(t):
+    t.down()
+    t.begin_fill()
     for _ in range(4):
-        turtle.forward(20)
-        turtle.left(90)
-    turtle.end_fill()
+        t.forward(20)
+        t.left(90)
+    t.end_fill()
 
-def right_square(turtle):
-    turtle.down()
-    turtle.begin_fill()
+def right_square(t):
+    t.down()
+    t.begin_fill()
     for _ in range(4):
-        turtle.forward(20)
-        turtle.right(90)
-    turtle.end_fill()
+        t.forward(20)
+        t.right(90)
+    t.end_fill()
 
-def row_left_squares(turtle, number):
-    turtle.down()
-    turtle.begin_fill()
+def row_left_squares(t, number):
+    t.down()
+    t.begin_fill()
     for _ in range(number):
-        left_square(turtle)
-        turtle.forward(20)
-    turtle.end_fill()
+        left_square(t)
+        t.forward(20)
+    t.end_fill()
 
-def row_right_squares(turtle, number):
-    turtle.down()
-    turtle.begin_fill()
+def row_right_squares(t, number):
+    t.down()
+    t.begin_fill()
     for _ in range(number):
-        right_square(turtle)
-        turtle.forward(20)
-    turtle.end_fill()
+        right_square(t)
+        t.forward(20)
+    t.end_fill()
