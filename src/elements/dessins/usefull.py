90ba2808c7207c2327841449b9e005f39867a00a
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-04-26 17:18:44 +0200

    #18 ci usefull, billBalle, boo et carapace ont une taille variable de pixels

--- src/elements/dessins/usefull.py
+++ src/elements/dessins/usefull.py
@@ -1,87 +1,87 @@
-def low_traverse(t):
-    t.forward(20)
+def low_traverse(t, pixel_size):
+    t.forward(pixel_size)
     t.left(90)
-    t.forward(20)
+    t.forward(pixel_size)
 
-def long_left_diagonal_from_above(t):
-    t.forward(40)
+def long_left_diagonal_from_above(t, pixel_size):
+    t.forward(2*pixel_size)
     t.left(90)
-    t.forward(20)
+    t.forward(pixel_size)
 
-def from_above(t):
+def from_above(t, pixel_size):
     t.right(90)
-    t.forward(60)
+    t.forward(3*pixel_size)
     t.right(90)
 
-def walk_along_up_square_right(t):
+def walk_along_up_square_right(t, pixel_size):
     t.right(90)
-    t.forward(20)
-    t.left(90)
-
-def double_walk_along_up_square_right(t):
-    t.right(90)
-    t.forward(40)
+    t.forward(pixel_size)
     t.left(90)
 
-def walk_along_up_square_left(t):
+def double_walk_along_up_square_right(t, pixel_size):
+    t.right(90)
+    t.forward(2*pixel_size)
     t.left(90)
-    t.forward(20)
-    t.right(90)
 
-def double_walk_along_up_square_left(t):
+def walk_along_up_square_left(t, pixel_size):
     t.left(90)
-    t.forward(40)
+    t.forward(pixel_size)
     t.right(90)
 
-def long_left_diagonal(t):
+def double_walk_along_up_square_left(t, pixel_size):
     t.left(90)
-    t.forward(20)
-    t.right(90)
-    t.forward(40)
-
-def double_high_traverse(t):
-    t.forward(20)
-    double_walk_along_up_square_right(t)
-
-def double_right_linear_traverse(t):
-    t.right(90)
-    t.forward(40)
+    t.forward(2*pixel_size)
     t.right(90)
 
-def left_square(t):
+def long_left_diagonal(t, pixel_size):
+    t.left(90)
+    t.forward(pixel_size)
+    t.right(90)
+    t.forward(2*pixel_size)
+
+def double_high_traverse(t, pixel_size):
+    t.forward(pixel_size)
+    double_walk_along_up_square_right(t, pixel_size)
+
+def double_right_linear_traverse(t, pixel_size):
+    t.right(90)
+    t.forward(2*pixel_size)
+    t.right(90)
+
+def left_square(t, pixel_size):
     t.down()
     t.begin_fill()
     for _ in range(4):
-        t.forward(20)
+        t.forward(pixel_size)
         t.left(90)
     t.end_fill()
 
-def right_square(t):
+def right_square(t, pixel_size):
     t.down()
     t.begin_fill()
     for _ in range(4):
-        t.forward(20)
+        t.forward(pixel_size)
         t.right(90)
     t.end_fill()
 
-def row_left_squares(t, number):
+def row_left_squares(t, number, pixel_size):
     t.down()
     t.begin_fill()
     for _ in range(2):
-        t.forward(20*number)
+        t.forward(pixel_size*number)
         t.left(90)
-        t.forward(20)
+        t.forward(pixel_size)
         t.left(90)
     t.end_fill()
-    t.forward(20*number)
+    t.forward(pixel_size*number)
 
-def row_right_squares(t, number):
+def row_right_squares(t, number, pixel_size):
     t.down()
     t.begin_fill()
     for _ in range(2):
-        t.forward(20*number)
+        t.forward(pixel_size*number)
         t.right(90)
-        t.forward(20)
+        t.forward(pixel_size)
         t.right(90)
     t.end_fill()
-    t.forward(20*number)
+    t.forward(pixel_size*number)
